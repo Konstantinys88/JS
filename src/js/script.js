e4ab2875789632b79611a7f6b4c93bbbd8c1293f
Konstantin0 <maschkovc@yandex.ru>
2022-08-03 20:02:39 +0300

    практика 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,56 @@
 "use sсtrict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+    // практика ч.1 урок 19
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    monies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько его оцените',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько его оцените','');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     monies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
 
-personalMovieDB.monies[a] = b; //ключ a - значение b
-personalMovieDB.monies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?',''),
+//       b = prompt('На сколько его оцените',''),
+//       c = prompt('Один из последних просмотренных фильмов?',''),
+//       d = prompt('На сколько его оцените','');
 
-console.log(personalMovieDB);
+// personalMovieDB.monies[a] = b; //ключ a - значение b
+// personalMovieDB.monies[c] = d;
+
+// console.log(personalMovieDB);
+
+    // практика ч.2 урок 20
+
+if ('egor' == 'egor2') {
+    console.log('ok');
+} else {
+    console.log('Error');
+}
+
+const num = 100;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('big');
+// } else {
+//     console.log ('ok');
+// }
+
+// (num == 50) ? console.log('ok') : console.log('error');
+
+switch (num) {
+    case 49: console.log('k1');
+    break;
+    case 48: console.log('k2');
+    break;
+    case 50: console.log('k3');
+    break;
+    case 51: console.log('k4');
+    break;
+    default: console.log('noooooo');
+    break;
+}
