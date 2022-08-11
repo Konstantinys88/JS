d5ddcecd074d8df568c2155366b7d7dcd85c0f81
Konstantin0 <maschkovc@yandex.ru>
2022-08-11 20:52:22 +0300

    цыклы

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-"use sсtrict";
+"use strict";
 
     // практика ч.1 урок 19
 
@@ -57,19 +57,72 @@
 
         // практика ч.1 урок 2
 
-const hamburger = 0;
-const fries = null;
-const cola = 0;
+// const hamburger = 0;
+// const fries = null;
+// const cola = 0;
 
-console.log(hamburger || cola || fries );
+// console.log(hamburger || cola || fries );
 
-if (hamburger || cola || fries) {
-    console.log('eats');
-} else {
-    console.log('not eats');
+// if (hamburger || cola || fries) {
+//     console.log('eats');
+// } else {
+//     console.log('not eats');
+// }
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'rewfdvdrg');
+
+    // Циклы урок 22
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+
+for (let i = 5; i <= 10 ; i++ ){
+    console.log(i);
 }
 
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(0 && 'rewfdvdrg');
+for (let i = 20; i >= 10 ; i-- ){
+    if (i === 13){
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i <= 10 ; i++ ){
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+while (i <= 16){
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+        i++;
+    }
+}
+
