8184533773681079b970e42e9f2c521b335d854e
Konstantin0 <maschkovc@yandex.ru>
2022-08-15 21:17:40 +0300

    function

--- src/js/function.js
+++ src/js/function.js
@@ -39,3 +39,61 @@ const calc2 = (a, b) =>  {
 };
 
 calc2();
+
+const usdCurr = 28;
+const esrCurr = 38;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    // console.log(curr * amount);
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+promotion(convert(500, usdCurr));
+
+convert(500, usdCurr);
+convert(500, esrCurr);
+
+
+
+function sayHello(t) {
+    return 'привет ' + t;
+}
+console.log(sayHello('Антон'));
+
+
+
+function returnNeighboringNumbers(number) {
+    let arr = [];
+    arr[0] = number - 1;
+    arr[1] = number;
+    arr[2] = number + 1
+    
+    return arr;
+}
+console.log(returnNeighboringNumbers(5));
+
+
+
+function getMathResult(a, b) {
+    if (b <= 0 || typeof b != 'number') {
+        return a;
+    } else {
+        let res = '';
+        for (let i = 1;i <= b;i++) { // цикл будет повторятся пока i < или = b
+            if ( i === b) {
+                res = res + a * i;
+            } 
+            else {
+                res = res + a * i + '---';
+            }
+        }
+        return res;
+    }
+}
+
+console.log(getMathResult(2, 5));
