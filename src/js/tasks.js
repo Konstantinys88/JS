5b4ad4b154e6ebc0b07c5f2945d390cb6912771d
Konstantin0 <maschkovc@yandex.ru>
2022-08-20 19:53:58 +0300

    tasks

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -86,7 +86,51 @@ function showProgrammingLangs(plan) {
 
 console.log(showProgrammingLangs(personalPlanPeter));
 
+// Задачи: 11
+// 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
 
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let res = "Семья состоит из:";
+    if (arr.length > 0) {
+        arr.forEach(function(i) {
+            res += ` ${i}`;
+        });
+    } else {
+        res = "Семья пуста";
+    }
+    return res;
+}
+      
+
+console.log(showFamily(family));
+console.log(family.length);
+
+// 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+// Пример:
+// standardizeStrings(favoriteCities)  выведет в консоль
+// lisbon
+// rome
+// milan
+// dublin
+// Это частая задача в реальности, так как от пользователя нам могут прийти ответы в самых разных форматах. В том числе и с разными буквами :) Поэтому нам нужно привести строки в один формат для правильной работы.
+
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    // let res = ``;
+    arr.forEach(function(i){
+        // res += `${i.toLowerCase()}\n`;
+        console.log(`${i.toLowerCase()}`);
+    });
+    // return res;
+}
+
+standardizeStrings(favoriteCities);
 
 
 
